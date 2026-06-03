60314ecedd540ba15afa53511ca320b5eef659d1
Martin Shelley <Martin.Shelley@Vitality.co.uk>
2026-06-03 09:34:44 +0100

    Initial commit of Widgetbook web

---
+++ lib/button.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'package:ui_lib/ui_components_manager.dart';
+
+@widgetbook.UseCase(name: 'Primary', type: VGButton)
+Widget buildDefaultButtonUseCase(BuildContext context) {
+  final buttonText = context.knobs.string(label: 'Button Text', initialValue: 'Button');
+  final showLeadingIcon = context.knobs.boolean(label: 'Show Leading Icon', initialValue: false);
+  final showTrailingIcon = context.knobs.boolean(label: 'Show Trailing Icon', initialValue: false);
+  final buttonSize = context.knobs.list<String>(
+    label: 'Size',
+    options: ['Small', 'Large', 'Full Width'],
+    initialOption: 'Small'
+  );
+  final showDisabledState = context.knobs.boolean(label: 'Show Disabled State', initialValue: false);
+  // final additionalHorizontalPadding = context.knobs.double.input(label: 'Add additional horizontal padding', initialValue: 16);
+
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: Center(
+      child: VGButton.primary(
+        buttonText,
+        leadingIcon: showLeadingIcon ? Icons.chevron_left_sharp : null,
+        trailingIcon: showTrailingIcon ? Icons.add : null,
+        onTap: showDisabledState ? null : () {},
+        buttonSize: buttonSize == 'Small' ? VGButtonSize.small : buttonSize == 'Large' ? VGButtonSize.large : null,
+        fullWidth: buttonSize == 'Full Width' ? true : false,
+        // paddingHorizontal: additionalHorizontalPadding,
+      )
+    )
+  );
+}
+
+@widgetbook.UseCase(name: 'Secondary', type: VGButton)
+Widget buildSecondaryButtonUseCase(BuildContext context) {
+  final buttonText = context.knobs.string(label: 'Button Text', initialValue: 'Button');
+  final showLeadingIcon = context.knobs.boolean(label: 'Show Leading Icon', initialValue: false);
+  final showTrailingIcon = context.knobs.boolean(label: 'Show Trailing Icon', initialValue: false);
+  final buttonSize = context.knobs.list<String>(
+    label: 'Size',
+    options: ['Small', 'Large', 'Full Width'],
+    initialOption: 'Small'
+  );
+  final showDisabledState = context.knobs.boolean(label: 'Show Disabled State', initialValue: false);
+  // final additionalHorizontalPadding = context.knobs.double.input(label: 'Add additional horizontal padding', initialValue: 16);
+
+  return Scaffold(
+    backgroundColor: Colors.grey,
+    body: Center(
+      child: VGButton.secondary(
+        buttonText,
+        leadingIcon: showLeadingIcon ? Icons.chevron_left_sharp : null,
+        trailingIcon: showTrailingIcon ? Icons.add : null,
+        onTap: showDisabledState ? null : () {},
+        buttonSize: buttonSize == 'Small' ? VGButtonSize.small : buttonSize == 'Large' ? VGButtonSize.large : null,
+        fullWidth: buttonSize == 'Full Width' ? true : false,
+        // paddingHorizontal: additionalHorizontalPadding,
+      )
+    )
+  );
+}
+
+@widgetbook.UseCase(name: 'Outlined', type: VGButton)
+Widget buildOutlinedButtonUseCase(BuildContext context) {
+  final buttonText = context.knobs.string(label: 'Button Text', initialValue: 'Button');
+  final showLeadingIcon = context.knobs.boolean(label: 'Show Leading Icon', initialValue: false);
+  final showTrailingIcon = context.knobs.boolean(label: 'Show Trailing Icon', initialValue: false);
+  final buttonSize = context.knobs.list<String>(
+    label: 'Size',
+    options: ['Small', 'Large', 'Full Width'],
+    initialOption: 'Small'
+  );
+  final showDisabledState = context.knobs.boolean(label: 'Show Disabled State', initialValue: false);
+  // final additionalHorizontalPadding = context.knobs.double.input(label: 'Add additional horizontal padding', initialValue: 16);
+
+  return Scaffold(
+    backgroundColor: Colors.white,
+    body: Center(
+      child: VGButton.outlined(
+        buttonText,
+        leadingIcon: showLeadingIcon ? Icons.chevron_left_sharp : null,
+        trailingIcon: showTrailingIcon ? Icons.add : null,
+        onTap: showDisabledState ? null : () {},
+        buttonSize: buttonSize == 'Small' ? VGButtonSize.small : buttonSize == 'Large' ? VGButtonSize.large : null,
+        fullWidth: buttonSize == 'Full Width' ? true : false,
+        // paddingHorizontal: additionalHorizontalPadding,
+      )
+    )
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ui_lib/ui_components_manager.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'main.directories.g.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Your existing configuration step
+  VGApplicationTheme.setUp(null, null, null); 
+
+  runApp(const WidgetbookApp());
+}
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      lightTheme: ThemeData.light(),
+      directories: directories,
+      // addons: [],
+    );
+  }
+}
